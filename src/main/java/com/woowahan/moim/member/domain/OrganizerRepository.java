223d9541c313c892364964a7eca06430b79c07f1
Celina <luv.mins99@gmail.com>
2022-07-27 13:00:26 +0900

    feat: 주최자 / 참여자 관련 로직 변경(1)
    - 분리했던 객체(Organizer, Participant) 삭제

--- src/main/java/com/woowahan/moim/member/domain/OrganizerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.woowahan.moim.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
-}
--- src/main/java/com/woowahan/moim/member/domain/ParticipantRepository.java
+++
@@ -1,6 +0,0 @@
-package com.woowahan.moim.member.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParticipantRepository extends JpaRepository<Participant, Long> {
-}
