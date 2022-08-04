0de7f39c6ed49e54b108ee59e193cebe5d1b6824
Celina <luv.mins99@gmail.com>
2022-08-04 17:45:16 +0900

    test: 로그인 실패 테스트 구현

--- src/test/java/com/woowahan/moim/login/acceptance/LoginAcceptanceTest.java
+++ src/test/java/com/woowahan/moim/login/acceptance/LoginAcceptanceTest.java
@@ -1,9 +1,11 @@
 package com.woowahan.moim.login.acceptance;
 
 import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.invalidPassword;
+import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.organizerUser;
+import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.participantUser;
 import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.validPassword;
-import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.주최자를_추가한다;
-import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.참여자를_추가한다;
+import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.주최자를_생성한다;
+import static com.woowahan.moim.member.acceptance.MemberAcceptanceTest.참여자를_생성한다;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -19,21 +21,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-class LoginAcceptanceTest extends AcceptanceTest {
+public class LoginAcceptanceTest extends AcceptanceTest {
 
     @BeforeEach
     public void setUp() {
         super.setUp();
 
-        주최자를_추가한다(validPassword);
-        참여자를_추가한다(validPassword);
+        주최자를_생성한다(validPassword);
+        참여자를_생성한다(validPassword);
     }
 
     @DisplayName("주최자 로그인")
     @Test
     void organizer_login() {
         // given & when
-        ExtractableResponse<Response> response = 로그인_요청("organizer", validPassword);
+        ExtractableResponse<Response> response = 로그인_요청(organizerUser, validPassword);
 
         // then
         로그인_성공(response);
@@ -43,7 +45,7 @@ class LoginAcceptanceTest extends AcceptanceTest {
     @Test
     void organizer_login_fail() {
         // given & when
-        ExtractableResponse<Response> response = 로그인_요청("organizer", invalidPassword);
+        ExtractableResponse<Response> response = 로그인_요청(organizerUser, invalidPassword);
 
         // then
         로그인_실패(response);
@@ -53,7 +55,7 @@ class LoginAcceptanceTest extends AcceptanceTest {
     @Test
     void participant_login() {
         // given & when
-        ExtractableResponse<Response> response = 로그인_요청("participant", validPassword);
+        ExtractableResponse<Response> response = 로그인_요청(participantUser, validPassword);
 
         // then
         로그인_성공(response);
@@ -63,7 +65,17 @@ class LoginAcceptanceTest extends AcceptanceTest {
     @Test
     void participant_login_fail() {
         // given & when
-        ExtractableResponse<Response> response = 로그인_요청("participant", invalidPassword);
+        ExtractableResponse<Response> response = 로그인_요청(participantUser, invalidPassword);
+
+        // then
+        로그인_실패(response);
+    }
+
+    @DisplayName("존재하지 않는 계정 정보로 로그인시 로그인 실패")
+    @Test
+    void no_user_login_fail() {
+        // given & when
+        ExtractableResponse<Response> response = 로그인_요청("test", validPassword);
 
         // then
         로그인_실패(response);
